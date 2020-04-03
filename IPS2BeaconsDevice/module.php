895be238c641cd135fb21349f09b3d113add4348
Joey-1970 <paeper@horburg.de>
2020-04-03 22:00:46 +0200

    Update module.php

--- IPS2BeaconsDevice/module.php
+++ IPS2BeaconsDevice/module.php
@@ -9,8 +9,8 @@
             	// Diese Zeile nicht löschen.
             	parent::Create();
 		$this->RegisterPropertyBoolean("Open", false);
+		$this->ConnectParent("{1CB80BEC-EEB8-8CEB-F8A1-7DB11013F6A7}");
 		
-		$this->RequireParent("{BAB408E0-0A0F-48C3-B14E-9FB2FA81F66A}"); // Multicast Socket
 		
 		
         }
@@ -37,47 +37,6 @@
             	// Diese Zeile nicht löschen
             	parent::ApplyChanges();
 		
-		$ParentID = $this->GetParentID();
-			
-		If ($ParentID > 0) {
-			/*
-			If (IPS_GetProperty($ParentID, 'Host') <> $this->ReadPropertyString('IPAddress')) {
-				IPS_SetProperty($ParentID, 'Host', $this->ReadPropertyString('IPAddress'));
-			}
-			*/
-			If (IPS_GetProperty($ParentID, 'Port') <> 8173) {
-				IPS_SetProperty($ParentID, 'BindPort', 8173);
-			}
-			If (IPS_GetProperty($ParentID, 'Port') <> 8173) {
-				IPS_SetProperty($ParentID, 'BindPort', 8173);
-			}
-			If (IPS_GetProperty($ParentID, 'Open') <> $this->ReadPropertyBoolean("Open")) {
-				IPS_SetProperty($ParentID, 'Open', $this->ReadPropertyBoolean("Open"));
-			}
-			If (IPS_GetProperty($ParentID, 'EnableBroadcast') <> false) {
-				IPS_SetProperty($ParentID, 'EnableBroadcast', false);
-			}
-			If (IPS_GetProperty($ParentID, 'EnableLoopback') <> false) {
-				IPS_SetProperty($ParentID, 'EnableLoopback', false);
-			}
-			If (IPS_GetProperty($ParentID, 'EnableReuseAddress') <> false) {
-				IPS_SetProperty($ParentID, 'EnableReuseAddress', false);
-			}
-			If (substr(IPS_GetName($ParentID), 0, 16) == "Multicast Socket") {
-				IPS_SetName($ParentID, "IPS2Beacons (IPS2BeaconsSplitter #".$this->InstanceID.")");
-			}
-			if(IPS_HasChanges($ParentID))
-			{
-				$Result = @IPS_ApplyChanges($ParentID);
-				If ($Result) {
-					$this->SendDebug("ApplyChanges", "Einrichtung des Multicast Socket erfolgreich", 0);
-				}
-				else {
-					$this->SendDebug("ApplyChanges", "Einrichtung des Multicast Socket nicht erfolgreich!", 0);
-				}
-			}
-		}
-		
 		
 		If ($this->ReadPropertyBoolean("Open") == true) {
 			
@@ -91,36 +50,10 @@
 		}	
 	}
 	
-	public function ForwardData($JSONString) 
-	{
-	 	// Empfangene Daten von der Device Instanz
-	    	$data = json_decode($JSONString);
-	    	$Result = false;
-	 	switch ($data->Function) {
-			
-			
-		}
-	return $Result;
-	}
 	
-	public function ReceiveData($JSONString) {	
- 	    	// Empfangene Daten vom I/O
-	    	$Data = json_decode($JSONString);
-	    	$Buffer = utf8_decode($Data->Buffer); 
-		$ClientIP = $Data->ClientIP;
-		$ClientPort = $Data->ClientPort;
-		$this->SendDebug("ReceiveData", "Buffer: ".$Buffer." ClintIP: ".$ClientIP." ClientPort: ".$ClientPort, 0);
-		
-	}   
 	// Beginn der Funktionen
 	
-	
-	private function GetParentID()
-	{
-		$ParentID = (IPS_GetInstance($this->InstanceID)['ConnectionID']);  
-	return $ParentID;
-	}
-	   
+
 	 
 }
 ?>
